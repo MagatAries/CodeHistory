7bbdfe6dcf5b660be23fca6fa2c6c76a4e3c3fdc
Aries <acpm1999@gmail.com>
2022-05-03 22:46:14 -0400

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Aries Magat on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
